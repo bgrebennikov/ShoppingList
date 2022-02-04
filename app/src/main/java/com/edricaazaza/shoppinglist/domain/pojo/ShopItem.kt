4ab908193c1c0c5c4d6ca1f276cd86052f033c17
bgrebennikov <bgrebennikovv@gmail.com>
2022-02-04 16:12:41 +0400

    test solve for pojo: add undefiened id

--- app/src/main/java/com/edricaazaza/shoppinglist/domain/pojo/ShopItem.kt
+++ app/src/main/java/com/edricaazaza/shoppinglist/domain/pojo/ShopItem.kt
@@ -1,8 +1,13 @@
 package com.edricaazaza.shoppinglist.domain.pojo
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+
+    val id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
